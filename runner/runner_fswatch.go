4919b1841062719dd88439eb0285ff8cb4090bb6
Ulderico Cirello <ulderico.cirello@gmail.com>
2024-01-16 05:14:08 -0800

    runner: make poll mode the default

--- runner/runner_fswatch.go
+++ runner/runner_fswatch.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !poll
-// +build !poll
+//go:build fswatch
+// +build fswatch
 
 package runner
 
