69832a561a575157c5fe5d0f1d3da427c1e15066
Ulderico Cirello <ulderico.cirello@gmail.com>
2024-06-23 22:15:00 -0700

    runner: remove fswatch mode

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.21
 require (
 	cirello.io/oversight v1.3.0
 	github.com/buildkite/terminal-to-html v3.2.0+incompatible
-	github.com/fsnotify/fsnotify v1.7.0
 	github.com/google/go-cmp v0.6.0
 	github.com/urfave/cli v1.22.15
 	golang.org/x/tools v0.22.0
@@ -15,5 +14,4 @@ require (
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
 )
--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -27,6 +27,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"path/filepath"
 	"slices"
 	"strings"
 	"sync"
@@ -575,4 +576,53 @@ func (r *Runner) deleteServiceDiscovery(svc string) {
 	r.sdMu.Lock()
 	delete(r.dynamicServiceDiscovery, svc)
 	r.sdMu.Unlock()
+}
+
+func (s *Runner) monitorWorkDir(ctx context.Context) (<-chan string, error) {
+	if _, err := os.Stat(s.WorkDir); err != nil {
+		return nil, err
+	}
+	triggereds := make(chan string, 1)
+	memo := make(map[string]time.Time)
+	go func() {
+		for {
+			_ = filepath.Walk(s.WorkDir, func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				if info.IsDir() {
+					for _, skipDir := range s.SkipDirs {
+						if skipDir == "" {
+							continue
+						}
+						if strings.HasPrefix(path, filepath.Join(s.WorkDir, skipDir)) {
+							return filepath.SkipDir
+						}
+					}
+					return nil
+				}
+				for _, p := range s.Observables {
+					if match(p, path) {
+						mtime := info.ModTime()
+						memoMTime, ok := memo[path]
+						if !ok {
+							memo[path] = mtime
+							memoMTime = mtime
+						}
+						if !mtime.Equal(memoMTime) {
+							memo[path] = mtime
+							select {
+							case triggereds <- path:
+							default:
+							}
+							break
+						}
+					}
+				}
+				return nil
+			})
+		}
+	}()
+	go func() { triggereds <- "" }()
+	return triggereds, nil
 }
